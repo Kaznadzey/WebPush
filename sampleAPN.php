eaec43da77d7eea4c7c3131238c112306f487859
Nazar Kaznadzey <nazar.kaznadzey@gmail.com>
2017-10-30 21:04:07 +0200

    Add samples

---
+++ sampleAPN.php
@@ -0,0 +1,21 @@
+<?php
+
+use Nazz\WebPush\Sender\Client\APN;
+use Nazz\WebPush\Sender\Message;
+
+$client = new APN(
+    '',
+    123456789,
+    '',
+    30
+);
+
+$message = new Message(
+    md5(microtime(true)),
+    'Message title',
+    'Message body',
+    'https://firebase.google.com/_static/79b4008122/images/firebase/lockup.png',
+    'https://firebase.google.com'
+);
+
+var_dump($client->send('This is subscription token', $message));
---
+++ sampleHTTP.php
@@ -0,0 +1,21 @@
+<?php
+
+use Nazz\WebPush\Sender\Client\FirebaseHTTP;
+use Nazz\WebPush\Sender\Message;
+
+$client = new FirebaseHTTP(
+    'fcm-xmpp.googleapis.com',
+    'my-firebase-api-key'
+);
+
+$message = new Message(
+    md5(microtime(true)),
+    'Message title',
+    'Message body',
+    'https://firebase.google.com/_static/79b4008122/images/firebase/lockup.png',
+    'https://firebase.google.com'
+);
+
+var_dump($client->send('This is subscription token', $message));
+
+?>
