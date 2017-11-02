340f94eed3e3f2cd3954ffeb843c56a625774dad
Nazar Kaznadzey <nazar.kaznadzey@gmail.com>
2017-11-02 20:10:41 +0200

    Add nessage test

---
+++ tests/Sender/MessageTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Nazz\WebPush\Sender;
+
+use Nazz\WebPush\Sender\Message;
+
+/**
+ * Class MessageTest
+ */
+class MessageTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Message
+     */
+    public function testMessage()
+    {
+        $id    = 123;
+        $title = 'Title';
+        $body  = 'Body';
+        $icon  = 'http://www.google.com';
+        $url   = 'http://www.google.com';
+
+        $message = new Message(
+            $id,
+            $title,
+            $body,
+            $icon,
+            $url
+        );
+
+        $this->assertEquals($message->getId(), $id);
+        $this->assertEquals($message->getTitle(), $title);
+        $this->assertEquals($message->getBody(), $body);
+        $this->assertEquals($message->getIcon(), $icon);
+        $this->assertEquals($message->getUrl(), $url);
+    }
+}
